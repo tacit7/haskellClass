7737a53a481b02519e247eaafd8ff679b7447c74
Uriel Maldonado <uriel781@gmail.com>
2014-05-01 23:27:24 +0000

    Adding "the right solution"
    This is the solution from the class slides.  This solution creates a data class called unit which has a number of units. The unit data type then derives with the Ord and Eq type classes and then defines how each unit should be shown.
    
    The factor function defines the conversion constants. Note that it defines the last three units as a reciprocal of the first three units. The inverse class then maps a  unit to its reciprocal unit. Finally, the convert function returns a tuple  with the correct conversion.

--- src/Lec4Hw.hs
+++ src/Lec4Hw.hs
@@ -1,6 +1,18 @@
 module Lec4Hw where
--- See below for details
+-- Convert between metric and imperial.
+-- convert :: (Double, [Char]) -> (Double, [Char])
+--    m ↔ yd
+--    L ↔ gal
+--    kg ↔ lb
 
+-- Prelude> convert (1, "m")
+-- (1.09361, "yd")
+-- Prelude> convert (1, "L")
+--  (0.264172, "gal")
+-- Prelude> convert (1, "kg")
+-- (2.20462, "lb")
+
+-- My solution
 convert :: (Double, [Char]) -> (Double, [Char])
 convert (d, "yd") = ( metersToYards d, "m")
 convert (d, "m")  = ( yardsToMeters d, "yd")
@@ -21,25 +33,48 @@ y2m = 1.09361
 l2g = 0.264172
 
 
+-- The right solution from the class slides
 
+data Unit = Meter -- Unit as in measurement and not ()
+          | Yard
+          | Litre
+          | Gallon
+          | Kilogram
+          | Pound
+      deriving (Ord, Eq) -- <-- class types
+instance Show Unit where
+  show Meter    = "m"
+  show Yard     = "yd"
+  show Litre    = "l"
+  show Gallon   = "gal"
+  show Kilogram = "kg"
+  show Pound    = "lb"
 
+-- map the factors to each unit
+factor :: Unit -> Double
+factor Meter    = 1.09361
+factor Litre    = 0.264172
+factor Kilogram = 2.20462
+factor Yard     = 1 / (factor Meter)
+factor Gallon   = 1 / (factor Litre)
+factor Pound    = 1 / (factor Kilogram)
 
+-- map the the units with their reciprocal unit
+inverse :: Unit -> Unit
+inverse Meter    = Yard
+inverse Yard     = Meter
+inverse Litre    = Gallon
+inverse Gallon   = Litre
+inverse Kilogram = Pound
+inverse Pound    = Kilogram
 
+convert2 :: (Double, Unit) -> (Double, Unit) -- using convert2 because convert is taken already
+convert2 (n, u) = (n * (factor u), inverse u)
+--                (n * factorConst, theInverseUnit)
 
+data Measurement = Measurement Double Unit deriving (Eq)
+instance Show Measurement where
+  show (Measurement n u) = (show n) ++ " " ++ (show u)
 
--- Convert between metric and imperial.
-
--- convert :: (Double, [Char]) -> (Double, [Char])
-
---    m ↔ yd
---    L ↔ gal
---    kg ↔ lb
-
-
--- Prelude> convert (1, "m")
--- (1.09361, "yd")
--- Prelude> convert (1, "L")
---  (0.264172, "gal")
--- Prelude> convert (1, "kg")
--- (2.20462, "lb")
-
+conv (Measurement n u) = measurement $ convert2 (n, u)
+      where measurement (n1, u1) = Measurement n1 u1
